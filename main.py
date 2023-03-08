1e634b4fce28483adf673da148a041d4b007bb2f
leo.reinmann <leo@reinmann.me>
2023-03-08 20:46:52 +0000

    Title and year regex ready, reading mkv also ready

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+import enzyme
 import os
 
 
 
 if __name__ == '__main__':
-    rootdir = 'movies'
+    rootdir = '/home/host/movies'
 
     for subdir, dirs, files in os.walk(rootdir):
-        text = subdir.rsplit('/', 1)[0]
-        print(subdir)
+        name = str(subdir.rsplit('/', 1)[-1]) # Title (YEAR)
+        title = ''
+        year = ''
+        if '(' in name:
+            year = name.split('(', 1)[-1][:-1]
+            title = name.split('(', 1)[0]
+
+
+        if files:
+            print(title)
+            with open(str(subdir) + '/' + str(files[0]), 'rb') as f:
+                mkv = enzyme.MKV(f)
+
+                audio = mkv.audio_tracks
+                video = mkv.video_tracks
+                print(str(video))
